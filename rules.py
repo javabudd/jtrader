2cadb9a068a44df0c9f592d8630e20814ea5aa57
andyo <javabudd@gmail.com>
2021-05-27 21:25:38 -0600

    remove rules

--- rules.py
+++
@@ -1,8 +0,0 @@
-import requests
-from secrets import IEX_CLOUD_API_TOKEN
-
-api_url = f'https://cloud.iexapis.com/stable/rules/schema?token={IEX_CLOUD_API_TOKEN}'
-response = requests.get(api_url)
-data = response.json()
-
-print(data)
