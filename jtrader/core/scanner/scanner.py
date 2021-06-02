e9314622a77e68382b798c58183cfe5f1c4eb418
andyo <javabudd@gmail.com>
2021-06-01 19:22:41 -0600

    Add scanner threading

--- jtrader/core/scanner/scanner.py
+++ jtrader/core/scanner/scanner.py
@@ -1,3 +1,4 @@
+import _thread
 import time
 
 import pandas as pd
@@ -8,7 +9,7 @@ from jtrader.core.iex import IEX
 from jtrader.core.validator.apo import APOValidator
 from jtrader.core.validator.ultosc import ULTOSCValidator
 
-stocks = pd.read_csv('files/all_stocks.csv')
+stocks = pd.read_csv('files/all_stocks.csv', chunksize=5000)
 
 
 class Scanner(IEX):
@@ -19,23 +20,31 @@ class Scanner(IEX):
         ]
 
         while True:
-            randomized_stocks = stocks.sample(frac=1)
+            for chunk in enumerate(stocks):
+                _thread.start_new_thread(self.loop, (indicators, chunk))
 
-            for ticker in randomized_stocks['Ticker']:
-                self.logger.info('Processing ticker: ' + ticker)
-                for indicator in indicators:
-                    indicator_instance = indicator(ticker, self.iex_client)
-                    try:
-                        resolved_indicator = indicator_instance.validate()
-                    except PyEXception as e:
-                        self.logger.error(e.args[0] + ' ' + e.args[1])
-
-                        break
-                    if resolved_indicator:
-                        found_indicator = indicator.get_name()
-                        message = ticker + ' triggered ' + found_indicator
-                        self.logger.info(message)
-                        utils.send_slack_message(message, '#stock-scanner')
-
-            utils.send_slack_message('Scanner pass-through, sleeping for 60 minutes', '#stock-scanner')
             time.sleep(3600)
+
+    def loop(self, indicators, chunk):
+        sleep = .5
+
+        if self.is_sandbox:
+            sleep = 2
+
+        for ticker in chunk[1]['Ticker']:
+            self.logger.info('Processing ticker: ' + ticker)
+            for indicator in indicators:
+                indicator_instance = indicator(ticker, self.iex_client)
+                try:
+                    resolved_indicator = indicator_instance.validate()
+                except PyEXception as e:
+                    self.logger.error(e.args[0] + ' ' + e.args[1])
+
+                    break
+                if resolved_indicator:
+                    found_indicator = indicator.get_name()
+                    message = ticker + ' triggered ' + found_indicator
+                    self.logger.info(message)
+                    utils.send_slack_message(message, '#stock-scanner')
+
+            time.sleep(sleep)
