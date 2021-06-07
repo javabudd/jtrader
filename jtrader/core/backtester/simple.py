7320a9ebf4978b8cc9440ef316af51b671cda2d7
andyo <javabudd@gmail.com>
2021-06-07 17:38:04 -0600

    Adjust backtester strat

--- jtrader/core/backtester/simple.py
+++ jtrader/core/backtester/simple.py
@@ -8,29 +8,26 @@ from jtrader.core.validator.volume import VolumeValidator
 
 def initialize(context):
     context.i = 0
-    context.asset = symbol('MSFT')
+    context.asset = symbol('AMZN')
 
 
 def handle_data(context, data: BarData):
-    # Skip first 300 days to get full windows
+    bar_count = 100
     context.i += 1
-    if context.i < 300:
+    if context.i < bar_count:
         return
 
-    high = data.history(context.asset, 'high', bar_count=100, frequency="1d")
-    low = data.history(context.asset, 'low', bar_count=100, frequency="1d")
-    close = data.history(context.asset, 'close', bar_count=100, frequency="1d")
-    volume = data.history(context.asset, 'volume', bar_count=100, frequency="1d")
+    high = data.history(context.asset, 'high', bar_count=bar_count, frequency="1d")
+    low = data.history(context.asset, 'low', bar_count=bar_count, frequency="1d")
+    close = data.history(context.asset, 'close', bar_count=bar_count, frequency="1d")
+    volume = data.history(context.asset, 'volume', bar_count=bar_count, frequency="1d")
 
     rsi_validator = RSIValidator(context.asset)
     volume_validator = VolumeValidator(context.asset)
     data_frame = pd.DataFrame({"high": high, "low": low, "close": close, "volume": volume})
 
-    is_rsi_valid = rsi_validator.is_valid(data_frame)
-    is_volume_valid = volume_validator.is_valid(data_frame)
-
-    if is_rsi_valid and is_volume_valid:
-        order_target(context.asset, 1000)
+    if rsi_validator.is_valid(data_frame):
+        order_target(context.asset, 5000)
     else:
         volume_validator.is_bullish = False
         rsi_validator.is_bullish = False
@@ -38,4 +35,4 @@ def handle_data(context, data: BarData):
             order_target(context.asset, 0)
 
     # Save values for later inspection
-    record(MSFT=data.current(context.asset, 'price'))
+    record(Asset=data.current(context.asset, 'close'))
