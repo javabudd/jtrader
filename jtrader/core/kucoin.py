d82218005b0f1951cc99a7a4c24f1cf61118e863
andyo <javabudd@gmail.com>
2021-11-10 10:42:12 -0700

    Add kucoin client to requirements. Persist websocket items into previous dataset

--- jtrader/core/kucoin.py
+++ jtrader/core/kucoin.py
@@ -1,5 +1,6 @@
 import json
 import random
+from datetime import datetime, timedelta
 
 import pandas as pd
 import websocket
@@ -17,6 +18,7 @@ class KuCoin:
         self.provider = provider
         self.has_initial_dataset = False
         self.ticker = ticker
+        self.frames = None
 
     def subscribe(self) -> None:
         self.provider.connect_websocket(self.on_websocket_open, self.on_websocket_message)
@@ -26,24 +28,39 @@ class KuCoin:
             print('candle added...')
             candles = candle_data['data']['candles']
             start_time = candles[0]
-            previous_frames = pd.DataFrame(None, None, self.KLINE_COLUMNS)
 
-            if not self.has_initial_dataset:
+            if self.frames is None:
                 print('looking up previous data...')
                 previous = self.get_previous_dataset(start_time)
-                if start_time != previous[0][0]:
-                    previous.insert(0, candles)
 
-                previous_frames = pd.concat([pd.DataFrame(previous, columns=self.KLINE_COLUMNS)], ignore_index=True)
-                self.has_initial_dataset = True
+                self.frames = pd.concat(
+                    [pd.DataFrame(previous, columns=self.KLINE_COLUMNS)],
+                    ignore_index=True
+                )
+
+            latest_item = self.frames.iloc[0]
+
+            if start_time != latest_item['date']:
+                item = {
+                    "date": start_time,
+                    "open": candles[1],
+                    "close": candles[2],
+                    "high": candles[3],
+                    "low": candles[4],
+                    "volume": candles[5],
+                    "amount": candles[6]
+                }
+                self.frames = self.frames.append(item, ignore_index=True)
+                self.frames.sort_values(by=['date'], inplace=True, ascending=False)
+                self.frames.reset_index(inplace=True, drop=True)
 
             macd = MACD(self.ticker)
             rsi = RSI(self.ticker)
             lr = LinearRegression(self.ticker)
 
-            print('MACD Signal: ', macd.is_valid(previous_frames))
-            print('RSI Signal: ', rsi.is_valid(previous_frames))
-            print('LR Signal: ', lr.is_valid(previous_frames))
+            print('MACD Signal:', macd.is_valid(self.frames))
+            print('RSI Signal:', rsi.is_valid(self.frames))
+            print('LR Signal:', lr.is_valid(self.frames))
 
         try:
             data = json.loads(message)
@@ -67,8 +84,12 @@ class KuCoin:
         socket.send(json.dumps(data))
 
     def get_previous_dataset(self, dataset_start_time: str) -> list:
+        date = datetime.fromtimestamp(int(dataset_start_time))
+
+        start = date - timedelta(days=1)
+
         rest_response = self.provider.client.get(
-            self.provider.url + f"/market/candles?type=1min&symbol={self.ticker}&startsAt={dataset_start_time}"
+            self.provider.url + f"/market/candles?type=1min&symbol={self.ticker}&startsAt={start.microsecond}"
         )
 
         try:
