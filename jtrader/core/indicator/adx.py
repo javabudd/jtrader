83e08fee0852eff3fdef40cdf7b2e501a34936f3
Andy OBrien <javabudd@gmail.com>
2021-11-11 20:19:26 -0700

    Add positive and negative DI convergence

--- jtrader/core/indicator/adx.py
+++ jtrader/core/indicator/adx.py
@@ -16,11 +16,13 @@ class ADX(Indicator):
 
     Buy signals:
 
-    Wilder suggests that a strong trend is present when ADX is above 25 and no trend is present when below 20.
+    If the +DI line crosses above the -DI line and the ADX is above 20, or ideally above 25, then that is a potential
+    signal to buy.
 
     Sell signals:
 
-    Wilder suggests that a strong trend is present when ADX is above 25 and no trend is present when below 20.
+    If the -DI crosses above the +DI, and the ADX is above 20 or 25, then that is an opportunity to enter a potential
+    short trade.
 
     When the ADX turns down from high values, then the trend may be ending. You may want to do additional research to
     determine if closing open positions is appropriate for you.
@@ -35,24 +37,25 @@ class ADX(Indicator):
 
     def is_valid(self, data, comparison_data=None):
         adx = talib.ADX(data['high'], data['low'], data['close'], timeperiod=self.time_period)
+        plus_di = talib.PLUS_DI(data['high'], data['low'], data['close'], timeperiod=self.time_period)
+        minus_di = talib.MINUS_DI(data['high'], data['low'], data['close'], timeperiod=self.time_period)
 
         self.clean_dataframe(adx)
+        self.clean_dataframe(plus_di)
+        self.clean_dataframe(minus_di)
 
-        if adx.empty:
+        if adx.empty or plus_di.empty or minus_di.empty:
             self.log_invalid_chart_length()
 
             return
 
         last_adx: float = adx.iloc[0]
-        average = adx.iloc[1:6]
-        average_adx = average.mean()
-
-        if last_adx >= 25 and average_adx < 25:
+        if plus_di.iloc[1] < minus_di.iloc[1] and plus_di.iloc[0] > minus_di.iloc[0] and last_adx >= 25:
             self.result_info['adx'] = round(last_adx, 2)
 
             return self.BULLISH
 
-        if last_adx <= 20 and average_adx > 20:
+        if minus_di.iloc[1] < plus_di.iloc[1] and minus_di.iloc[0] > plus_di.iloc[0] and last_adx >= 25:
             self.result_info['adx'] = round(last_adx, 2)
 
             return self.BEARISH
